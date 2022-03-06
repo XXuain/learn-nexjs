421046dc4991273256e1c6463872db9598062ec5
yellowHuang <hlady1115@gmail.com>
2022-03-06 20:11:38 +0800

    add use Link comp enables client-side navigation

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import Link from 'next/link';
+import { Fragment } from 'react';
 
 function News() {
-  return <div>News</div>;
+  return (
+    <Fragment>
+      <h1>News</h1>
+      <ul>
+        <li>
+          <Link href="/news/12">12</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default News;
