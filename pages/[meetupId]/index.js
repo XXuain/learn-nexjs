0ab2b577f2add3afe895a95d4b81916df9d12e77
XXuain <hlady1115@gmail.com>
2022-04-24 15:46:00 +0800

    add: getStacticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,7 +1,33 @@
+/** @format */
+
 import React from 'react';
 
 function MeetupDetail() {
-  return <div>index</div>;
+  return <div>MeetupDetail</div>;
+}
+
+export async function getStaticPaths() {
+  // pre-generation dynamic page
+  return {
+    fallback: false, // true is dynamically
+    paths: [{ params: { meetupId: 'm1' } }, { params: { meetupId: 'm2' } }],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log('meetupId', meetupId);
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: 'mmm2222',
+        image:
+          'https://image.kkday.com/v2/image/get/w_960%2Cc_fit%2Cq_55%2Ct_webp/s1.kkday.com/product_24157/20210302064554_MlaQe/jpg',
+        address: 'this is address222',
+      },
+    },
+  };
 }
 
 export default MeetupDetail;
