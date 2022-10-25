dcb434e4244f07d62fb2ac4b4fcad21070b58014
yellowHuang <yellow@iyes.dev>
2022-10-25 14:14:28 +0800

    update: fetch data use fs.readFile

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,10 @@
 /** @format */
-import { MongoClient } from 'mongodb';
 import Head from 'next/head';
-import MeetupList from '../components/meetups/MeetupList';
-
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'mmm111',
-    image:
-      'https://image.kkday.com/v2/image/get/w_960%2Cc_fit%2Cq_55%2Ct_webp/s1.kkday.com/product_24157/20210302064554_MlaQe/jpg',
-    address: 'this is address',
-  },
-  {
-    id: 'm2',
-    title: 'mmm2222',
-    image:
-      'https://image.kkday.com/v2/image/get/w_960%2Cc_fit%2Cq_55%2Ct_webp/s1.kkday.com/product_24157/20210302064554_MlaQe/jpg',
-    address: 'this is address222',
-  },
-];
+import fs from 'fs/promises'; // file system
+import path from 'path';
 
 function Home(props) {
+  console.log('props', props);
   return (
     <>
       <Head>
@@ -30,38 +14,17 @@ function Home(props) {
           content={'Browser a huge list of highly active React neetups!'}
         />
       </Head>
-      <MeetupList meetups={props.meetups} />
     </>
   );
 }
 
-// export async function getServerSideProps(context) {
-//   const req = context.req;
-//   const res = context.res;
-//   // fetch data for api
-//   return {
-//     props: { meetups: DUMMY_MEETUPS },
-//   };
-// }
-
 export async function getStaticProps() {
-  // get db
-  const client = await MongoClient.connect('mongodb+srv://xxxx');
-  const db = client.db();
-  const meetupsCollection = db.collection('meetups');
-  const meetups = await meetupsCollection.find().toArray();
-  client.close();
+  const filePath = path.join(process.cwd(), 'data', 'dummy_products.json');
+  const data = await fs.readFile(filePath);
+  const jsonData = JSON.parse(data);
 
   return {
-    props: {
-      meetups: meetups.map((meetup) => ({
-        title: meetup.title,
-        address: meetup.address,
-        image: meetup.image,
-        id: meetup._id.toString(),
-      })),
-    },
-    revalidate: 1, // second
+    props: { products: jsonData.products },
   };
 }
 
--- pages/news/[newsId].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from 'next/router';
-function NewsItem() {
-  const router = useRouter();
-  const newsId = router.query.newsId;
-  return <div>newsId: {newsId}</div>;
-}
-
-export default NewsItem;
