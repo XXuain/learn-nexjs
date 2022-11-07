9bedc54caa1f3cc723b4ea869163e8497660bf4d
XXuain <hlady1115@gmail.com>
2022-11-07 15:35:24 +0800

    add: search

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,34 @@
 /** @format */
-
-import React from 'react';
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import { getEventById } from '../../data/dummy-data';
+import EventSummary from 'components/event-detail/event-summary';
+import EventLogistics from 'components/event-detail/event-logistics';
+import EventContent from 'components/event-detail/event-content';
 
 function EventDetail() {
-  return <div>EventDetail</div>;
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return 'ne event';
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
 
 export default EventDetail;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,23 @@
 /** @format */
 
-import React from 'react';
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import { getAllEvents } from '../../data/dummy-data';
 import EventList from 'components/events/event-list';
-import { getFeaturedEvents } from '../../data/dummy-data';
+import EventsSearch from 'components/events/events-search';
+
 function Events() {
-  const featuredEvents = getFeaturedEvents();
-  return <EventList items={featuredEvents} />;
+  const router = useRouter();
+  const featuredEvents = getAllEvents();
+  const onSearchHandle = (year, month) => {
+    const fullPath = `events/${year}/${month}`;
+    router.push(fullPath);
+  };
+  return (
+    <Fragment>
+      <EventsSearch onSearch={onSearchHandle} />
+      <EventList items={featuredEvents} />;
+    </Fragment>
+  );
 }
 export default Events;
